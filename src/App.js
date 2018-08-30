482dfc771e5881d990fbf19402af55e2447d1d7f
Patrick Williams <pwmckenna@gmail.com>
2018-08-30 05:32:46 -0700

    flexbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import React from 'react';
 
-const photos = [
-    { src: require('./IMG_4148.JPG'), width: '100%' },
-    { src: require('./IMG_4139.JPG'), width: '100%' },
-    { src: require('./IMG_4132.JPG'), width: '100%' },
-    { src: require('./IMG_4119.JPG'), width: '100%' },
-    { src: require('./IMG_4102.JPG'), width: '100%' },
-    { src: require('./IMG_4074.JPG'), width: '100%' },
-    { src: require('./IMG_4018.JPG'), width: '100%' },
-    { src: require('./IMG_4017.JPG'), width: '100%' },
-    { src: require('./IMG_4016.JPG'), width: '100%' },
-    { src: require('./IMG_3991.JPG'), width: '50%' },
-    { src: require('./IMG_3992.JPG'), width: '50%' },
-]
+const Row = ({ children }) => (
+    <div style={{
+        display: 'flex',
+        flex: 1,
+        border: '10px solid #eee',
+        margin: '1em',
+        alignItems: 'center'
+    }}>
+        {children}
+    </div>
+);
+
+const Image = props => (
+    <img {...props} style={{ width: '100%' }}/>
+);
 
 export default () => (
-    <div>
-        {photos.map((photo, i) => (
-            <img {...photo}/>
-        ))}
+    <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%'
+    }}>
+        <Row>
+            <Image src={require('./IMG_4148.JPG')}/>
+        </Row>
+        <Row>
+            <Image src={require('./IMG_4139.JPG')}/>
+        </Row>
+        <Row>
+            <Image src={require('./IMG_4132.JPG')}/>
+        </Row>
+        <Row>
+            <Image src={require('./IMG_4119.JPG')}/>
+        </Row>
+        <Row>
+            <Image src={require('./IMG_4102.JPG')}/>
+        </Row>
+        <Row>
+            <Image src={require('./IMG_4074.JPG')}/>
+        </Row>
+        <Row>
+            <Image src={require('./IMG_4018.JPG')}/>
+        </Row>
+        <Row>
+            <Image src={require('./IMG_4017.JPG')}/>
+        </Row>
+        <Row>
+            <Image src={require('./IMG_4016.JPG')}/>
+        </Row>
+        <Row>
+            <Image src={require('./IMG_3992.JPG')}/>
+            <Image src={require('./IMG_3991.JPG')}/>
+        </Row>
     </div>
 )
